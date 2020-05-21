5ae24f955da6c7811dfa7a0e97eed6d740d2de50
neeraja <nanil@tesla.com>
2020-05-21 10:53:21 -0700

    convert to django version 1.11.15

--- setup.py
+++ setup.py
@@ -18,13 +18,13 @@ CLASSIFIERS=[
 ]
 
 setup(
-    name='django-pyodbc-azure',
+    name='zipline-django-pyodbc',
     version='1.11.15.0',
-    description='Django backend for Microsoft SQL Server and Azure SQL Database using pyodbc',
+    description='Django backend for Microsoft SQL Server using pyodbc forked from django-pyodbc-azure',
     long_description=open('README.rst').read(),
-    author='Michiya Takahashi',
-    author_email='michiya.takahashi@gmail.com',
-    url='https://github.com/michiya/django-pyodbc-azure',
+    author='Michiya Takahashi , modified by Neeraja',
+    author_email='neeraja.anil2012@gmail.com',
+    url='https://github.com/neerajaanil/zipline-django-pyodbc-azure',
     license='BSD',
     packages=['sql_server', 'sql_server.pyodbc'],
     install_requires=[
